2f430415fbdd9c10897b3aac19c5963c663f66b8
Marcos Naves <f12684146896@nbsp148029.DASA.NET>
2014-08-25 16:47:03 -0300

    Configuração do banco postgress Criação das classes para buscar localidade de internacionalização.

---
+++ sysglazing/src/main/java/br/com/ln/comum/BeanVar.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.com.ln.comum;
+
+import java.io.Serializable;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import java.util.Locale;
+
+/**
+ * Definição de váriavies para visualização no modo web.
+ * @author Marcos Naves
+ */
+
+@SessionScoped
+@ManagedBean(name = "beanVar")
+public class BeanVar implements Serializable{
+    
+    private String localizacao;
+
+    public BeanVar() {
+        this.localizacao = VarComuns.local();
+    }
+        
+    public String getLocalizacao() {
+        return localizacao;
+    }
+
+    public void setLocalizacao(String localizacao) {
+        this.localizacao = localizacao;
+    }
+    
+    
+}
---
+++ sysglazing/src/main/java/br/com/ln/comum/VarComuns.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.com.ln.comum;
+
+import java.io.Serializable;
+import java.util.Locale;
+
+/**
+ *
+ * @author Marcos Naves
+ */
+public class VarComuns implements Serializable{
+    
+    public static String local(){
+        Locale locale = Locale.getDefault();
+        return locale.toString();
+    }
+    
+}
