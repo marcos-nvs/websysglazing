ab6f748e18558c7bd01900b6af230d7f599d4d4d
Marcos Naves <f12684146896@nbsp148029.DASA.NET>
2014-08-26 16:51:26 -0300

    Configuração do banco de dados, preparação para criação de seções no banco, realização de testes

---
+++ sysglazing/src/main/java/Teste.java
@@ -0,0 +1,38 @@
+
+import br.com.ln.hibernate.SessionFactoryDbName;
+import java.util.List;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Marcos Naves
+ */
+public class Teste {
+    
+    public static void main(String... args) {
+        
+//        Session session = SessionFactoryDbName.getCurrentSessionByName("Mille");
+//        Transaction tx = session.beginTransaction();
+//        
+//        GlaUsuario glaUsuarios = null;
+//        Query query = session.getNamedQuery("GlaUsuario.findAll");
+//        
+//        List l = query.list();
+//        
+//        if (l != null){
+//            glaUsuarios = (GlaUsuario) l.get(0);
+//            
+//            System.out.println("usuario : " + glaUsuarios.toString());
+//        } else {
+//            System.out.println("lista vazia.....");
+//        }
+    }
+}
