d258fe1fa035c9d98f7292dc7af0f5840e53ab83
Marcos Naves <f12684146896@nbsp148029.DASA.NET>
2014-09-11 19:41:48 -0300

    tela de cadastro

---
+++ sysglazing/src/main/java/br/com/ln/views/UsuarioView.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package br.com.ln.views;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import org.primefaces.model.TreeNode;
+
+/**
+ *
+ * @author Marcos Naves
+ */
+
+@ViewScoped
+@ManagedBean(name = "usuarioView")
+public class UsuarioView {
+    
+    private TreeNode lnRoot;
+    
+    
+    
+}
