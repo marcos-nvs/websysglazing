9b16d4acc26e2c535f27bac8bb98e5ad3b933d82
Marcos Naves <f12684146896@nbsp148029.DASA.NET>
2014-09-15 16:52:02 -0300

    tela de cadastro

--- sysglazing/src/main/java/br/com/ln/views/UsuarioView.java
+++ sysglazing/src/main/java/br/com/ln/views/UsuarioView.java
@@ -8,9 +8,14 @@ package br.com.ln.views;
 
 import br.com.ln.entity.LnUsuario;
 import br.com.ln.hibernate.Postgress;
+import java.io.Serializable;
 import java.util.List;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import org.primefaces.event.SelectEvent;
+import org.primefaces.event.UnselectEvent;
 
 /**
  *
@@ -19,9 +24,10 @@ import javax.faces.bean.ViewScoped;
 
 @ViewScoped
 @ManagedBean(name = "usuarioView")
-public class UsuarioView {
+public class UsuarioView implements Serializable{
     
     private List<LnUsuario> listUsuario = Postgress.getListObject(LnUsuario.class, "Public");
+    private LnUsuario lnUsuario;
 
     public UsuarioView() {
     }
@@ -33,7 +39,24 @@ public class UsuarioView {
     public void setListUsuario(List<LnUsuario> listUsuario) {
         this.listUsuario = listUsuario;
     }
-    
-    
-    
+
+    public LnUsuario getLnUsuario() {
+        return lnUsuario;
+    }
+
+    public void setLnUsuario(LnUsuario lnUsuario) {
+        this.lnUsuario = lnUsuario;
+    }
+
+    public void onRowSelect(SelectEvent event) {
+
+        lnUsuario = (LnUsuario) event.getObject();
+        System.out.println("Usuário selecionado : " + lnUsuario.getUsuStCodigo());
+        
+    }
+
+    public void onRowUnselect(UnselectEvent event) {
+        FacesMessage msg = new FacesMessage("Usuario não selecionado", ((LnUsuario) event.getObject()).getUsuStCodigo());
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+    }
 }
